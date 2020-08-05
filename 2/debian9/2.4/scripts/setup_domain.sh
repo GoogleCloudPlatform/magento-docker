74d3725c3a0d6a9a323c51d1c1291c99eeb0ebd0
Armando Miani <armadom@google.com>
2020-08-05 14:58:08 +0200

    [magento] Docker images updates - 2020-08-05

---
+++ 2/debian9/2.4/scripts/setup_domain.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Copyright 2019 Google LLC
+#
+# This software is licensed under the Open Software License version
+# 3.0. The full text of this license can be found in https://opensource.org/licenses/OSL-3.0
+# or in the file LICENSE which is distributed along with the software.
+
+set -eu
+
+DOMAIN_NAME="$1"
+
+if [[ -z "${DOMAIN_NAME}" ]]; then
+    echo >&2 "DOMAIN_NAME var should be provided."
+    exit 1
+do
+
+cd /app
+
+bin/magento setup:store-config:set --base-url="http://${DOMAIN_NAME}/"
+    && bin/magento cache:flush
+
+echo "======================"
+echo "URLs Configured."
+echo "http://${DOMAIN_NAME}"
+echo "======================"
